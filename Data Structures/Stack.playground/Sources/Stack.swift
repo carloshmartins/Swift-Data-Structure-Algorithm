59dc6e4637cfbf0ffe290a02077ca9a42915ae08
carlos.henrique.martrins <carlos.henrique.martrins@MacBook-Pro-de-globo.local>
2021-03-24 22:24:25 -0300

    create Stack Implementation

---
+++ Data Structures/Stack.playground/Sources/Stack.swift
@@ -0,0 +1,57 @@
+/*
+ There are only two essential operations for a stack:
+ • push: Adding an element to the top of the stack.
+ • pop: Removing the top element of the stack.
+ 
+ LIFO
+ 
+ Examples:
+    - Navigation Stack
+    - Memory Allocation
+    - Search and Conquer Algorithms, to find a path out of a maze, facilitate backtracking
+ 
+ Complexity (Push and Pop) ->>>>  O(1) time complexity
+ 
+ Topics Learned:
+ - Generics in Struct
+ - Mutating Function
+ - Discardable Result
+ - CustomStringConvertible
+*/
+
+import Foundation
+
+public struct Stack<Element>: ExpressibleByArrayLiteral {
+    
+    public typealias ArrayLiteralElement = Element
+    
+    private var storage: [Element] = []
+    
+    public init() {}
+    
+    public init(arrayLiteral elements: Element...) {
+        storage = elements
+    }
+    
+    public mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    @discardableResult
+    public mutating func pop() -> Element? {
+        storage.popLast()
+    }
+}
+
+extension Stack: CustomStringConvertible {
+    
+    public var description: String {
+        """
+        ----top----
+        \(storage.map { "\($0)" }.reversed().joined(separator:
+        "\n"))
+        -----------
+        """
+    }
+}
+
