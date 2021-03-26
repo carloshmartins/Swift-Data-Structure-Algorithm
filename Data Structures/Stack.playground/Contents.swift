18c771e4df293b92aea6cb8308467bc872f1d15f
Carlos Henrique <juliaomartinsch@gmail.com>
2021-03-25 21:40:29 -0300

    solve challenge 1 for Stack

--- Data Structures/Stack.playground/Contents.swift
+++ Data Structures/Stack.playground/Contents.swift
@@ -1,17 +1,34 @@
 import Foundation
 
-var stack = Stack<Int>()
+/*
+ Challenge 1: Reverse an Array
+ Create a function that prints the contents of an array in reversed order.
 
-stack.push(1)
-stack.push(2)
-stack.push(3)
-stack.push(4)
+ */
 
-print(stack)
-
-
-if let poppedElement = stack.pop() {
-  assert(4 == poppedElement)
-  print("Popped: \(poppedElement)")
+func reverseArray<T>(array: [T]) {
+    var nonReverdedStack = Stack<T>()
+    var reverdedStack = Stack<T>()
+    
+    for index in array {
+        nonReverdedStack.push(index)
+    }
+    print(nonReverdedStack.description)
+    
+    for _ in array {
+        reverdedStack.push(nonReverdedStack.pop()!)
+    }
+    
+    print(reverdedStack.description)
 }
+
+reverseArray(array: [1,2,3,4,5])
+
+/*
+ Challenge 2: Balance the parentheses
+ Check for balanced parentheses. Given a string, check if there are ( and ) characters, and return true if the parentheses in the string are balanced. For example:
+
+ */
+
+
 
